101ed663da00f14a82e6a2757991c09f11a87359
Utilisateur <theo.benmoussa@hotmail.fr>
2025-07-28 16:00:07 +0200

    affichage en cours liaison react + fastapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, use} from "react"
 import api from './api'
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [formPokemon, setFormPokemon] = useState({
+    name: '',
+    type: [],
+    hp: '',
+    attack: '',
+    weakness: '',
+  });
 
+  const fetchPokemons = async() => {
+    const response = await api.get('/total_pokemons');
+    setPokemons(response.data)
+  };
+
+  useEffect(() => {
+    fetchPokemons(); 
+  }, []);
+
+  return (
+    <div>
+      <nav className="navbar ,navbar-dark bg primarys">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Pokemons
+          </a>
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default App;
